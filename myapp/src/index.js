b6c07e16ef38d82a494bdc8afc29f6d111f72415
Sean Yang <eiffel205566@hotmail.com>
2023-05-28 15:45:19 -0400

    try out burrito tutorial

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { FirebaseAppProvider } from "reactfire";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const firebaseConfig = {
+  apiKey: "AIzaSyDqfbPmzqrOXOJ8uExsvR4LEuWeRHPxdUQ",
+  authDomain: "nnnn-5269d.firebaseapp.com",
+  projectId: "nnnn-5269d",
+  storageBucket: "nnnn-5269d.appspot.com",
+  messagingSenderId: "1013498764474",
+  appId: "1:1013498764474:web:0c41e0c52ac5055bc8f683",
+  measurementId: "G-DFCPW7L211",
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </FirebaseAppProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
