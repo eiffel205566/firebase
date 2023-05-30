67172eeca95f9c5b96731a669e8cf8ab8d5e6ea0
Sean Yang <eiffel205566@hotmail.com>
2023-05-30 13:13:39 -0400

    update LoginPage

--- myapp/src/pages/LoginPage.tsx
+++ myapp/src/pages/LoginPage.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { useAuth } from "reactfire";
 
-import { useNavigate } from "react-router-dom";
-import { signIn, signOut } from "../components/AppContent.tsx";
+import { Navigate } from "react-router-dom";
+import { signIn } from "../components/AppContent.tsx";
 
 const LoginPage = ({ user, status }) => {
   const auth = useAuth();
-  const navigate = useNavigate();
   const isUserDefined = status === "success" && user != null;
   const isSigningIn = status === "loading" && user == null;
   const isSigningOut = status === "loading" && user != null;
@@ -16,20 +15,22 @@ const LoginPage = ({ user, status }) => {
     ? "Signing out"
     : `Click to Sign ${isUserDefined ? "Out" : "In"}`;
 
-  return (
-    <div title='Sign-in form'>
-      <button
-        onClick={() => {
-          isUserDefined
-            ? signOut(auth)
-            : signIn(auth).then(() => navigate("/home"));
-        }}
-        disabled={status === "loading"}
-      >
-        {message}
-      </button>
-    </div>
-  );
+  if (isUserDefined) {
+    return <Navigate to='/home' replace />;
+  } else {
+    return (
+      <div title='Sign-in form'>
+        <button
+          onClick={() => {
+            signIn(auth);
+          }}
+          disabled={status === "loading"}
+        >
+          {message}
+        </button>
+      </div>
+    );
+  }
 };
 
 export default LoginPage;
