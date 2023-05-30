5824ed6fa9275a41d196be979c0d47bd7c1d30a4
Sean Yang <eiffel205566@hotmail.com>
2023-05-30 13:52:03 -0400

    move user logging in logic to AppContent

--- myapp/src/components/AppContent.tsx
+++ myapp/src/components/AppContent.tsx
@@ -1,6 +1,9 @@
-import React from "react";
-import { useSigninCheck, SigninCheckResult } from "reactfire";
+import React, { useEffect, useRef } from "react";
+import { useSigninCheck, SigninCheckResult, useFirestore } from "reactfire";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { collection, doc, where, getDocs, setDoc } from "firebase/firestore";
+import { query } from "firebase/database";
+
 import HomePage from "../pages/HomePage.tsx";
 import LoginPage from "../pages/LoginPage.tsx";
 
@@ -8,6 +11,32 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 export const AppContent = () => {
   const { data: signInCheckResult, status } = useSigninCheck();
+  const firestore = useFirestore();
+  const onlineUsersRef = collection(firestore, "onlineUsers");
+  const userAdditionRef = useRef(false);
+
+  // add logged in user to db only once to ensure we can query all onlined users
+  useEffect(() => {
+    (async () => {
+      if (!signInCheckResult?.user) return;
+
+      const userQuery = query(
+        collection(firestore, "onlineUsers"),
+        where("uid", "==", signInCheckResult?.user?.uid ?? "")
+      );
+      const userQuerySnapshot = await getDocs(userQuery);
+
+      if (userQuerySnapshot.empty && !userAdditionRef.current) {
+        userAdditionRef.current = true;
+        const { uid, displayName: userName } = signInCheckResult?.user ?? {};
+        await setDoc(doc(onlineUsersRef), {
+          uid,
+          userName,
+        });
+      }
+      // setUserLoggedIn(true);
+    })();
+  }, [signInCheckResult?.user]);
 
   return (
     <BrowserRouter>
--- myapp/src/pages/HomePage.tsx
+++ myapp/src/pages/HomePage.tsx
@@ -1,33 +1,15 @@
-import React, { useEffect, useState, useRef } from "react";
+import React from "react";
 import {
-  getDatabase,
-  ref,
   query,
-  orderByChild,
-  push,
-  set,
-  increment as rtdbIncrement,
 } from "firebase/database";
 import {
   useAuth,
-  useSigninCheck,
   SigninCheckResult,
   useFirestore,
-  useFirestoreDocData,
-  useDatabase,
-  useDatabaseListData,
   useFirestoreCollectionData,
 } from "reactfire";
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import {
   collection,
-  doc,
-  where,
-  getFirestore,
-  orderBy,
-  getDocs,
-  getDoc,
-  setDoc,
 } from "firebase/firestore";
 import { signOut } from "../components/AppContent.tsx";
 
@@ -42,38 +24,11 @@ const HomePage = ({
   const onlineUsersQuery = query(onlineUsersRef);
   const onlineUsersQueryResult = useFirestoreCollectionData(onlineUsersQuery);
 
-  const [isUserLoggedIn, setUserLoggedIn] = useState(false);
-  const userAdditionRef = useRef(false);
-
-  // add logged in user to db only once to ensure we can query all onlined users
-  useEffect(() => {
-    (async () => {
-      const userQuery = query(
-        collection(firestore, "onlineUsers"),
-        where("uid", "==", user?.uid ?? "")
-      );
-      const userQuerySnapshot = await getDocs(userQuery);
-
-      if (userQuerySnapshot.empty && !userAdditionRef.current) {
-        userAdditionRef.current = true;
-        const { uid, displayName: userName } = user ?? {};
-        await setDoc(doc(onlineUsersRef), {
-          uid,
-          userName,
-        });
-      }
-      setUserLoggedIn(true);
-    })();
-  }, []);
-
-  if (!isUserLoggedIn || onlineUsersQueryResult.status === "loading")
-    return <>Loading...</>;
-
   return (
     <div title='Sign Out'>
       {user && <div>{`Hello, ${user.displayName}`}</div>}
       <button onClick={() => signOut(auth)}>Sign out</button>
-      {onlineUsersQueryResult.data.map((d, index) => (
+      {(onlineUsersQueryResult?.data ?? []).map((d, index) => (
         <div key={index}>{d.userName}</div>
       ))}
     </div>
