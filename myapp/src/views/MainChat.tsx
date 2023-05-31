fc56f1741e2efbb96d116b836df9b972341fa414
Sean Yang <eiffel205566@hotmail.com>
2023-05-30 22:29:50 -0400

    setup textarea

--- myapp/src/views/MainChat.tsx
+++ myapp/src/views/MainChat.tsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 const MainChat = () => {
+  const [message, setMessage] = useState<string>("");
+
   return (
     <div className='chatContainer relative bg-gray-500 flex-grow'>
       <div className='absolute bottom-[10%] left-[50%] bg-gray-600'>
-        <input />
+        <form onSubmit={() => {}}>
+          <div>
+            <label htmlFor='message'>Message:</label>
+            <textarea
+              className='text-gray-700'
+              id='message'
+              value={message}
+              onChange={e => setMessage(e.target.value)}
+            />
+          </div>
+          <button
+            className='hover:bg-gray-700 text-white py-2 px-4 rounded'
+            type='submit'
+          >
+            Submit
+          </button>
+        </form>
       </div>
     </div>
   );
