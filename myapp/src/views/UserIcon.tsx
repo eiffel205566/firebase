65dbe2c8f37841b91c53b8587d68ad05d827fafb
Sean Yang <eiffel205566@hotmail.com>
2023-05-31 23:53:42 -0400

    update UserIcon color

--- myapp/src/views/UserIcon.tsx
+++ myapp/src/views/UserIcon.tsx
@@ -1,9 +1,13 @@
+import React from "react";
+
 const UserIcon = ({
   isMe,
   name,
+  isPlaceholder,
 }: {
   isMe: boolean;
   name: { firstLetter: string; secondLetter: string };
+  isPlaceholder: boolean;
 }) => {
   const finalName = isMe
     ? "Me"
@@ -11,10 +15,12 @@ const UserIcon = ({
         name.secondLetter ? ` ${name.secondLetter}` : ""
       }`;
 
-  return (
+  return isPlaceholder ? (
     <div className='h-[30px] w-[30px] rounded-full bg-gray-700 flex items-center justify-center'>
       <span className='text-white text-[10px] font-bold'>{finalName}</span>
     </div>
+  ) : (
+    <div className='h-[30px] w-[30px]' />
   );
 };
 
