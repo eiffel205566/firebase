6469e4c3770542d2a383acfd4a694d5ea8dab366
Sean Yang <eiffel205566@hotmail.com>
2023-05-31 23:18:07 -0400

    update login

--- myapp/src/pages/LoginPage.tsx
+++ myapp/src/pages/LoginPage.tsx
@@ -3,31 +3,30 @@ import { useAuth } from "reactfire";
 
 import { Navigate } from "react-router-dom";
 import { signIn } from "../components/AppContent.tsx";
+import login from "../views/login.jpg";
+import Letter from "../views/Letter.js";
 
 const LoginPage = ({ user, status }) => {
   const auth = useAuth();
   const isUserDefined = status === "success" && user != null;
-  const isSigningIn = status === "loading" && user == null;
-  const isSigningOut = status === "loading" && user != null;
-  const message = isSigningIn
-    ? "Signing in"
-    : isSigningOut
-    ? "Signing out"
-    : `Click to Sign ${isUserDefined ? "Out" : "In"}`;
 
   if (isUserDefined) {
     return <Navigate to='/home' replace />;
   } else {
     return (
-      <div className='loginPage h-full w-full bg-gray-500' title='Sign-in form'>
-        <button
-          onClick={() => {
-            signIn(auth);
-          }}
-          disabled={status === "loading"}
-        >
-          {message}
-        </button>
+      <div className='loginPage h-full w-full'>
+        <img className='absolute h-full w-full blur-sm' src={login} />
+        <div className='h-full w-full flex justify-center'>
+          <button
+            className='z-10'
+            onClick={() => {
+              signIn(auth);
+            }}
+            disabled={status === "loading"}
+          >
+            Click Here to Sign In ChatRoom
+          </button>
+        </div>
       </div>
     );
   }
