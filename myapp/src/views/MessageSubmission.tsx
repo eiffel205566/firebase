eb405132e7318d91b2960ea942dc76b05d85945f
Sean Yang <eiffel205566@hotmail.com>
2023-06-01 09:43:27 -0400

    update update modal style

--- myapp/src/views/MessageSubmission.tsx
+++ myapp/src/views/MessageSubmission.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const MessageSubmission = ({ onSubmit, message, onChange }) => {
   return (
-    <div className='w-full bg-blue-100 fixed bottom-0'>
+    <div className='w-full fixed bottom-0'>
       <div className='bg-gray-600'>
         <form onSubmit={onSubmit}>
           <div
