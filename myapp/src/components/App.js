9a86db3a9d3d1e7cb537187b1fa86afc60000829
Sean Yang <eiffel205566@hotmail.com>
2023-06-03 11:59:13 -0400

    format App.js

--- myapp/src/components/App.js
+++ myapp/src/components/App.js
@@ -1,6 +1,6 @@
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
-import {  getFirestore } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 import {
   FirestoreProvider,
   useFirebaseApp,
@@ -11,11 +11,10 @@ import { AppContent } from "./AppContent.tsx";
 
 function App() {
   const app = useFirebaseApp();
-
   const database = getDatabase(app);
   const auth = getAuth(app);
-
   const firestoreInstance = getFirestore(useFirebaseApp());
+
   return (
     <AuthProvider sdk={auth}>
       <DatabaseProvider sdk={database}>
